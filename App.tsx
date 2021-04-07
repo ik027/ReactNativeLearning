57b58b4702e07f79c5362f85323f86cdbb002265
ik027 <dorayaki2015@gmail.com>
2021-04-07 22:20:32 +0900

    タイマー用ボタンを用意した

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity onPress={()=>console.log('pressed')}>
+        <Text>Timer</Text>
+      </TouchableOpacity>
     </View>
   );
 }
